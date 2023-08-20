1604db3c7ae7355881b0ec7809cdb9cddd63f864
Alwin Simon <003alwin@gmail.com>
2023-08-20 18:36:44 +0530

    Jwt token generation middleware configured

---
+++ backend/utils/jwtConfig/generateToken.js
@@ -0,0 +1,26 @@
+//? ===================================================== JWT Configuration =====================================================
+
+import jwt from 'jsonwebtoken';
+
+
+const generateToken = (res, userId) => {
+
+    // Creating a new json webtoken with userId and secret key
+    const jwtToken = jwt.sign({userId}, process.env.JWT_SECRET_KEY, { expiresIn: process.env.JWT_TOKEN_DURATION } );
+
+    const cookieOptions = {
+
+        httpOnly: true, // To prevent cookies from being accessed by client-side scripts
+        secure: process.env.NODE_ENV !== 'development', // Value will be false in the development environment and hence http will be allowed in development
+        sameSite: 'strict',
+        maxAge: 30 * 24 * 60 * 60 * 1000 // Sets expiry of cookie to 30 days
+
+    };
+
+    res.cookie('jwt', jwtToken, cookieOptions); 
+
+};
+
+
+
+export default generateToken;
