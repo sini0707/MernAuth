1ca739352378a707705fb58dc3820f74c8a6053d
Alwin Simon <003alwin@gmail.com>
2023-08-21 18:38:23 +0530

    Configured Sign-up page view

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import HomeScreen from './screens/HomeScreen.jsx';
 import LoginScreen from './screens/LoginScreen.jsx';
+import RegisterScreen from './screens/RegisterScreen.jsx';
 
 
 
@@ -16,8 +17,10 @@ const router = createBrowserRouter(
     <Route path='/' element={ <App/> } >
 
       <Route index={true} path='/' element={ <HomeScreen /> } />
-      
+
       <Route path='/login' element={ <LoginScreen /> } />
+
+      <Route path='/register' element={ <RegisterScreen /> } />
 
     </Route>
 
