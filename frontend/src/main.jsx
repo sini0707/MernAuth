f7d7cbb0efec681c167a011a893084c97136b7c2
Alwin Simon <003alwin@gmail.com>
2023-08-21 19:22:56 +0530

    Configured Redux Store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import store from './store.js';
+import { Provider } from 'react-redux';
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
@@ -31,7 +33,15 @@ const router = createBrowserRouter(
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={ router } />
-  </React.StrictMode>,
-)
+  
+  <Provider store={store} >
+
+    <React.StrictMode>
+      
+      <RouterProvider router={ router } />
+
+    </React.StrictMode>
+
+  </Provider>
+
+);
