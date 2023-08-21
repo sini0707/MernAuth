d0ee8448cec6864c8fc27f81d98562b6b7c5ee48
Alwin Simon <003alwin@gmail.com>
2023-08-21 18:24:30 +0530

    Configured Login page view

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import HomeScreen from './screens/HomeScreen.jsx';
+import LoginScreen from './screens/LoginScreen.jsx';
 
 
 
@@ -15,6 +16,8 @@ const router = createBrowserRouter(
     <Route path='/' element={ <App/> } >
 
       <Route index={true} path='/' element={ <HomeScreen /> } />
+      
+      <Route path='/login' element={ <LoginScreen /> } />
 
     </Route>
 
