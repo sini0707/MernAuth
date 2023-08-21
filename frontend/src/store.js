0ed019728e3c6c93b0bb2aef1b1930895d835603
Alwin Simon <003alwin@gmail.com>
2023-08-21 19:51:49 +0530

    Configured Auth Slice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from './slices/authSlice.js';
 
 const store = configureStore({
 
-    reducer: {},
+    reducer: {
+        auth: authReducer
+    },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
     devTools: true
 
