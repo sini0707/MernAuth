9143d06163cf67b97bd129b37d14706adb317b13
Alwin Simon <003alwin@gmail.com>
2023-08-21 11:26:51 +0530

    Configured user authentication middleware.

---
+++ backend/middlewares/userAuthMiddleware.js
@@ -0,0 +1,49 @@
+//? ===================================================== User Authentication Middleware =====================================================
+
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel';
+
+
+
+const authenticateUser = asyncHandler( async (req, res, next) => {
+
+    const tokenFromRequest = req.cookies.jwt;
+
+    if (tokenFromRequest) {
+    
+        try {
+            
+            const decodedTokenData = jwt.verify( tokenFromRequest, process.env.JWT_SECRET_KEY);
+
+            const requestUser = await User.findById(decodedTokenData.userId).select('-password');
+
+            if (requestUser) {
+            
+                req.user = requestUser;
+
+                next();
+
+            }
+
+        } catch (error) {
+            
+            res.status(401);
+
+            throw new Error(`Authentication Failed. Invalid token found`);
+
+        }
+
+    } else {
+
+        res.status(401);
+
+        throw new Error(`Authentication Failed. No token found`);
+
+    }
+
+});
+
+
+export default authenticateUser;
+
