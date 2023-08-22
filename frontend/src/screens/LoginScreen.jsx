307779920370b8443ea7dd61e771abcc915db48e
Alwin Simon <003alwin@gmail.com>
2023-08-22 11:58:42 +0530

    Configured login form submission to backend server

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,15 +1,52 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Form, Button, Row, Col } from "react-bootstrap";
+
+import { useDispatch, useSelector } from "react-redux";
+import { useLoginMutation } from "../slices/userApiSlice";
+import { setCredentials } from "../slices/authSlice";
+
+
 import FormContainer from "../components/FormContainer";
 
 const LoginScreen = () => {
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const { userInfo } = useSelector( (state) => state.auth );
+
+  useEffect( () => {
+
+    if(userInfo) {
+
+      navigate('/');
+
+    }
+
+  }, [ navigate, userInfo ] );
+
   const submitHandler = async (e) => {
+
     e.preventDefault();
-    console.log("submit");
+
+    try {
+      
+      const responseFromApiCall = await login( { email, password } );
+
+      setCredentials( { ...responseFromApiCall } );
+      
+    }catch(err){
+
+      console.log( err?.data?.message || err.error );
+
+    }
+
   };
 
   return (
