85100d1b3d53af03e03e1138cc4aac27516fc8ae
Alwin Simon <003alwin@gmail.com>
2023-08-23 19:45:31 +0530

    Modified user home and header content

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -40,7 +40,7 @@ const Header = () => {
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand>Compass Manager</Navbar.Brand>
+            <Navbar.Brand>Compass User</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
