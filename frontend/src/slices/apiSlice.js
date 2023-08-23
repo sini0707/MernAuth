591a40f10883f8d7ca344834156c63fb40bf57d7
Alwin Simon <003alwin@gmail.com>
2023-08-23 10:49:46 +0530

    Minor Fix - Removed an wrong import

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,4 +1,4 @@
-import { createApi, fakeBaseQuery, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({ baseQuery: '' });
 
