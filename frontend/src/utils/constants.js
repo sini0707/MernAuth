6e6ee4103aed1d3fda1a79ee5eeaca5096cb8bb6
Alwin Simon <003alwin@gmail.com>
2023-08-23 18:04:44 +0530

    Configured Admin Front-end with basic functionalities

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -5,3 +5,9 @@ export const USER_AUTHENTICATION_URL = '/api/users/auth'
 export const USER_LOGOUT_URL = '/api/users/logout'
 export const USER_REGISTRATION_URL = '/api/users'
 export const USER_PROFILE_URL = '/api/users/profile'
+
+
+export const ADMIN_AUTHENTICATION_URL = '/api/admin/auth'
+export const ADMIN_LOGOUT_URL = '/api/admin/logout'
+export const ADMIN_REGISTRATION_URL = '/api/admin'
+export const ADMIN_PROFILE_URL = '/api/admin/profile'
