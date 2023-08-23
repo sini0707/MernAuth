96bdabfbd64ec1a56845d684bc56ac61524db65d
Alwin Simon <003alwin@gmail.com>
2023-08-23 20:22:17 +0530

    Configured multer

---
+++ backend/config/multerConfig.js
@@ -0,0 +1,30 @@
+import multer from "multer";
+
+
+const storage = multer.diskStorage({
+
+  destination: (req, file, cb) => { cb(null, "backend/Public/UserProfileImages") },
+
+  filename: (req, file, cb) => { cb( null, file.fieldname + "_" + Date.now() + path.extname(file.originalname) ) }
+
+});
+
+
+const fileFilter = (req, file, cb) => {
+
+  if (file.mimetype.startsWith("image/")) {
+    
+    cb(null, true);
+
+  } else {
+
+    cb(new Error("Only images are allowed!"), false);
+
+  }
+
+};
+
+export const multerUploadUserProfile = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+});
