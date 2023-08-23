5f8a712f4a618cc66130667a1366d093e60e8fe0
Alwin Simon <003alwin@gmail.com>
2023-08-23 10:26:34 +0530

    Minor non-coding fix

--- backend/middlewares/adminAuthMiddleware.js
+++ backend/middlewares/adminAuthMiddleware.js
@@ -1,4 +1,4 @@
-//? ===================================================== User Authentication Middleware =====================================================
+//? ===================================================== Admin Authentication Middleware =====================================================
 
 import jwt from 'jsonwebtoken';
 import asyncHandler from 'express-async-handler';
